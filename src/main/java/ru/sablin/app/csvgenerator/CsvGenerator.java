df60a12895087cbce14891a8a17223d5a608a6f4
Sablin Alex <hdxucydheh@gmail.com>
2024-04-05 22:49:19 +0700

    corrected comments

--- src/main/java/ru/sablin/app/csvgenerator/CsvGenerator.java
+++ src/main/java/ru/sablin/app/csvgenerator/CsvGenerator.java
@@ -1,17 +1,16 @@
 package ru.sablin.app.csvgenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Data
-@AllArgsConstructor
 public class CsvGenerator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var list = List.of(
                 new Person("Alex", 12),
                 new Person("Sem", 34));
@@ -19,41 +18,39 @@ public class CsvGenerator {
         generateCsvFile(list, "Test.csv");
     }
 
-    public static void generateCsvFile(List<?> objects, String outputPath) {
+    public static <T> void generateCsvFile(List<T> objects, String outputPath) throws IOException {
         // Создаем новый файл для записи CSV
         var file = new File(outputPath);
-        try (var bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+        var bufferedWriter = new BufferedWriter(new FileWriter(file));
 
-            // Получаем все классы объектов
-            var classes = objects.get(0).getClass();
-            var fields = classes.getDeclaredFields();
-            Set<String> properties = new HashSet<>();
-            for (Field field : fields) {
-                if (field.isAnnotationPresent(ExportToCsv.class)) {
-                    properties.add(field.getName());
-                }
+        // Получаем все классы объектов
+        var classes = objects.get(0).getClass();
+        var fields = classes.getDeclaredFields();
+        Set<String> properties = new HashSet<>();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(ExportToCsv.class)) {
+                properties.add(field.getName());
             }
+        }
 
-            // Записываем заголовок CSV файла
+        // Записываем заголовок CSV файла
+        for (String property : properties) {
+            bufferedWriter.write(property + "|");
+        }
+        bufferedWriter.newLine();
+
+        // Записываем данные каждого объекта
+        for (Object obj : objects) {
             for (String property : properties) {
-                bufferedWriter.write(property + "|");
+                try {
+                    var field = obj.getClass().getDeclaredField(property);
+                    bufferedWriter.write(field.get(obj) + ",");
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    throw new RuntimeException("Error accessing property '" + property + "'", e);
+                }
             }
             bufferedWriter.newLine();
-
-            // Записываем данные каждого объекта
-            for (Object obj : objects) {
-                for (String property : properties) {
-                    try {
-                        var field = obj.getClass().getDeclaredField(property);
-                        bufferedWriter.write(field.get(obj) + ",");
-                    } catch (NoSuchFieldException | IllegalAccessException e) {
-                        throw new RuntimeException("Error accessing property '" + property + "'", e);
-                    }
-                }
-                bufferedWriter.newLine();
-            }
-        } catch (IOException i) {
-            i.printStackTrace();
         }
+        bufferedWriter.close();
     }
 }
